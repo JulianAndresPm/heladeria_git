0edfd0aa1934c650d2c4c5208647bca153f7cbac
JulianPaz <julianpaz@udenar.edu.co>
2023-02-20 18:18:36 -0500

    feat: se termina vista home

--- src/scripts.sql
+++ src/scripts.sql
@@ -1,10 +1,9 @@
-SELECT * FROM db_ice.usuarios 
-WHERE usuario = 'admin' and password ='admin123*';
+-- CREACION DE TABLAS
 
 -- TABLA USUARIOS
 
 CREATE TABLE usuarios (
-  id INT(10) NOT NULL,
+  id INT(10) NOT NULL primary key,
   usuario VARCHAR(16) NOT NULL,
   password VARCHAR(60) NOT NULL,
   nombres VARCHAR(40) NOT NULL,
@@ -14,10 +13,13 @@ CREATE TABLE usuarios (
   correo VARCHAR(60) NOT NULL
 );
 
-ALTER TABLE usuarios ADD PRIMARY KEY (id);
-ALTER TABLE usuarios MODIFY id INT(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1;
-INSERT INTO db_ice.usuarios(usuario,password,nombres,apellidos,fechaNac,telefono,correo) 
-	VALUES('prueba','prueba123*','prueba','prueba',0,0,'');
+-- TABLA PRODUCTOS
+CREATE TABLE productos(
+	id INT(10) primary key,
+    NombreProducto VARCHAR(70),
+    disponibilidad INT(1),
+    stock INT(4)
+);
 
 -- TABLA CARRITO COMPRAS
 CREATE TABLE carrito(
@@ -26,7 +28,14 @@ CREATE TABLE carrito(
     cantidad INT(4) not null
 );
 
+-- LLAVES PRIMARIAS COM
+
 ALTER TABLE carrito ADD PRIMARY KEY (id_usuario,id_producto);
+
+
+-- MODIFICACIONES A TABLAS
+
+ALTER TABLE usuarios MODIFY id INT(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1;
 
 ALTER TABLE carrito ADD CONSTRAINT FOREIGN KEY(id_usuario) REFERENCES usuarios(id)
 ON DELETE CASCADE ON UPDATE CASCADE;
@@ -34,31 +43,19 @@ ON DELETE CASCADE ON UPDATE CASCADE;
 ALTER TABLE carrito ADD CONSTRAINT FOREIGN KEY(id_producto) REFERENCES productos(id)
 ON DELETE CASCADE ON UPDATE CASCADE;
 
+ALTER TABLE productos MODIFY id INT(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1;
+
+ALTER TABLE productos ADD COLUMN precioxUni INT(10);
+
+-- INSERCION DE DATOS
+
+
+INSERT INTO db_ice.usuarios(usuario,password,nombres,apellidos,fechaNac,telefono,correo) 
+	VALUES('prueba','prueba123*','prueba','prueba',0,0,'');
+
 INSERT INTO db_ice.carrito(id_usuario,id_producto,cantidad) 
 	VALUES(2,1,3);
     
-select * from carrito;
-
-
--- TABLA PRODUCTOS
-CREATE TABLE productos(
-	id INT(10),
-    NombreProducto VARCHAR(70),
-    disponibilidad INT(1),
-    stock INT(4)
-);
-
-ALTER TABLE productos ADD PRIMARY KEY (id);
-ALTER TABLE productos MODIFY id INT(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 1;
 INSERT INTO db_ice.productos(NombreProducto,disponibilidad,stock) 
 	VALUES('Cokies & Cream',1,300);
-ALTER TABLE productos ADD COLUMN precioxUni INT(10);
-
-
-select * from productos;
-
-
-CREATE TABLE 
-
-
 
